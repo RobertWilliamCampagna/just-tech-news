551c811e791a4532d063d30d7637462cea5308d3
Robert Campagna <rwcnyla@gmail.com>
2021-01-31 21:12:35 -0800

    comments, upvote work

---
+++ __tests__/helpers.test.js
@@ -0,0 +1,56 @@
+const { COMMENT_KEYS } = require('@babel/types');
+const { test, expect } = require('@jest/globals');
+const {format_date, format_plural, format_url} = require('../utils/helpers');
+
+
+test('format_date() returns a date string', () => {
+    const date = new Date('2020-03-20 16:12:03');
+  
+    expect(format_date(date)).toBe('3/20/2020');
+  });
+
+test('makes word plural', () => {
+    console.log(format_plural("Tiger", 2));
+    expect(format_plural("Tiger", 2)).toBe("Tigers");
+})
+
+test('makes lion plural', () => {
+    console.log(format_plural("Lion", 2));
+    expect(format_plural("Lion", 2)).toBe("Lions");
+})
+
+test('make comment plural', () => {
+    console.log(format_plural("comment", 3));
+    expect(format_plural("comment", 3)).toBe("comments");
+})
+
+test('format_url() returns a simplified url string', () => {
+    const url1 = format_url('http://test.com/page/1');
+    const url2 = format_url('https://www.coolstuff.com/abcdefg/');
+    const url3 = format_url('https://www.google.com?q=hello');
+  
+    expect(url1).toBe('test.com');
+    expect(url2).toBe('coolstuff.com');
+    expect(url3).toBe('google.com');
+  });
+
+//   test('name to equal Robert', () => {
+//     const name = "Robert"
+  
+//     expect(name).toBe("Robert");
+//   });
+
+
+
+
+//   test('format_plural() returns a pluralized word, () => {
+//     const comment = new Comment({
+//         expect(comment)
+            
+//         } else {
+            
+//         }
+//     })
+    
+//     }
+//   });
